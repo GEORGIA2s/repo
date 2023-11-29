13b8e1bc85c5094b51187b84552c311a17cf72b1
GEORGIA2s <150633560+GEORGIA2s@users.noreply.github.com>
2023-11-29 11:08:44 +0900

    GameExample.java

--- src/study/level/middle/GameExample.java
+++ src/study/level/middle/GameExample.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 enum SkillType {
-    ATTACK,
-    DEFENSE,
-    HEALING
+    공격,
+    방어,
+    힐
 }
 
 class Character<T extends Skill> {
@@ -25,7 +25,7 @@ class Character<T extends Skill> {
     }
 
     public void useSkills() {
-        System.out.println(name + "'s skills:");
+        System.out.println(name + "의 스킬:");
 
         for (T skill : skills) {
             skill.use();
@@ -45,21 +45,21 @@ class Skill<T extends Enum<T>> {
     }
 
     public void use() {
-        System.out.println(name + " skill used with power " + power + " and type " + type);
+        System.out.println(name + " 스킬이 " + power + " 데미지를 입혔습니다. [Skill Type : " + type + "]");
     }
 }
 
 public class GameExample {
     public static void main(String[] args) {
-        Skill<SkillType> fireball = new Skill<>("Fireball", 50, SkillType.ATTACK);
-        Skill<SkillType> shield = new Skill<>("Shield", 30, SkillType.DEFENSE);
-        Skill<SkillType> healingWave = new Skill<>("Healing Wave", 20, SkillType.HEALING);
+        Skill<SkillType> fireball = new Skill<>("파이어볼", 50, SkillType.공격);
+        Skill<SkillType> shield = new Skill<>("쉴드", 30, SkillType.방어);
+        Skill<SkillType> healingWave = new Skill<>("힐링 웨이브", 20, SkillType.힐);
 
-        Character<Skill<SkillType>> mage = new Character<>("Mage", 10);
+        Character<Skill<SkillType>> mage = new Character<>("마법사", 10);
         mage.addSkill(fireball);
         mage.addSkill(healingWave);
 
-        Character<Skill<SkillType>> warrior = new Character<>("Warrior", 12);
+        Character<Skill<SkillType>> warrior = new Character<>("전사", 12);
         warrior.addSkill(shield);
 
         mage.useSkills();
